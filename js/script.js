0919d54e929f458ac5fc54e2b07d780f0d8fff4b
Amit Doliya <amitdoliya1994@gmail.com>
2024-01-27 16:18:30 +0530

    main new changes

--- js/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("clickText").addEventListener("click", function() {
-      var overlay = document.getElementById("overlay");
-      overlay.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-  });
-  
